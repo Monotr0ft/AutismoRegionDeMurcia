a5be8f32101b74fbd5f2dae652fe5411fffe99cd
Monotr0ft <garfremar@gmail.com>
2024-03-25 12:38:31 +0100

    Prueba rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/prueba', function() {
+    return 'Hola, si estás leyendo esto es porque la ruta funciona correctamente';
+});
