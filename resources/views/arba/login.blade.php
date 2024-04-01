0393fc683d6f456ed8f527a5f1a50ba6f7947fa1
Monotr0ft <garfremar@gmail.com>
2024-04-01 13:10:26 +0200

    Añadido bootstrap como view

--- resources/views/arba/login.blade.php
+++ resources/views/arba/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ARBA Inicio de Sesión</title>
+    @include('bootstrap.index')
     <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
 </head>
 <body>
