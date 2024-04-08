29232471d2a2bed1598b9fbb7533a4768ddac77e
Monotr0ft <garfremar@gmail.com>
2024-04-08 11:09:38 +0200

    Mejorado responsive pagina principal

--- resources/views/autismo/paginas/home.blade.php
+++ resources/views/autismo/paginas/home.blade.php
@@ -11,16 +11,16 @@
 <div>
     <div class="row">
         <h1 class="text-center my-5 col-12 text-decoration-underline">Autismo Región de Murcia</h1>
-        <div class="col-12 col-md-6">
+        <div class="col-12 col-md-6 order-2 order-md-1">
             <p class="justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ipsum ex dolores ut debitis, quod, aliquam sunt, praesentium maxime nulla repudiandae vero numquam quisquam suscipit adipisci dignissimos mollitia harum! Officia.
             Porro at numquam cum exercitationem pariatur adipisci ullam, repellat iusto aliquam est eius ducimus maxime harum sed possimus voluptatum labore vero ex perferendis expedita assumenda excepturi, magnam omnis! Praesentium, dolore?
             Ut possimus hic cum saepe deleniti vitae sed, deserunt quaerat impedit id voluptatum commodi? Soluta pariatur perspiciatis quasi a blanditiis inventore iure? Placeat recusandae nesciunt quidem dolore provident excepturi cumque?</p>
+            <div class="text-center m-2">
+                <a href="#"><button class="btn btn-primary">Más información</button></a>
+            </div>
         </div>
-        <div class="col-12 col-md-6 text-center">
+        <div class="col-12 col-md-6 order-1 order-md-2 text-center">
             <img src="{{ asset('assets/img/palma_autismo.jpg') }}" alt="Autismo" class="img-fluid" style="width: 30rem;">
-        </div>
-        <div class="col-12 col-md-6 m-2 text-center">
-            <a href="#"><button class="btn btn-primary">Más información</button></a>
         </div>
     </div>
     <div class="row">
@@ -33,7 +33,7 @@
             Porro at numquam cum exercitationem pariatur adipisci ullam, repellat iusto aliquam est eius ducimus maxime harum sed possimus voluptatum labore vero ex perferendis expedita assumenda excepturi, magnam omnis! Praesentium, dolore?
             Ut possimus hic cum saepe deleniti vitae sed, deserunt quaerat impedit id voluptatum commodi? Soluta pariatur perspiciatis quasi a blanditiis inventore iure? Placeat recusandae nesciunt quidem dolore provident excepturi cumque?</p>
             <div class="text-center m-2">
-            <a href="#"><button class="btn btn-primary">Más información</button></a>
+                <a href="#"><button class="btn btn-primary">Más información</button></a>
             </div>
         </div>
     </div>
