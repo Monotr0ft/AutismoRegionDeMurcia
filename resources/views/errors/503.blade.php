86b0bbbe78dd500446c76a8a25a0ab66f96f6910
Monotr0ft <garfremar@gmail.com>
2024-04-16 11:41:24 +0200

    Creada página error 503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Volveremos más tarde</title>
+    @include ('bootstrap.css')
+</head>
+<body>
+    @include ('bootstrap.script')
+</body>
+</html>
