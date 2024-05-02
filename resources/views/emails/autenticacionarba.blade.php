24e7986dfed33e0de2b63e9683c4a37517933d05
Monotr0ft <garfremar@gmail.com>
2024-05-02 12:05:45 +0200

    Arreglado

--- resources/views/emails/autenticacionarba.blade.php
+++ resources/views/emails/autenticacionarba.blade.php
@@ -1,30 +1,24 @@
 <x-mail::message>
 
-    <x-mail::header>
-        <x-mail::title>
-            Bienvenido a ARBA
-        </x-mail::title>
+    <x-mail::header url="">
+        Bienvenido a ARBA
     </x-mail::header>
 
-    <x-mail::body>
-        <p>
-            Hola {{ $nombre }} {{ $apellidos }}.
-        </p>
-        <p>
-            Se le ha añadido a ARBA como usuario para acceder a la plataforma.
-        </p>
-        <p>
-            Su contraseña es: <strong>{{ $password }}</strong>. Recuerde cambiarla en su primer inicio de sesión. Para cambiarla, inicie sesión en la plataforma y vaya a su perfil. Ahí, podrá cambiar su contraseña.
-        </p>
-        <p>
-            Esperemos que tenga una buena experiencia en la plataforma.
-        </p>
-    </x-mail::body>
+    <p>
+        Hola {{ $nombre }} {{ $apellidos }}.
+    </p>
+    <p>
+        Se le ha añadido a ARBA como usuario para acceder a la plataforma.
+    </p>
+    <p>
+        Su contraseña es: <strong>{{ $password }}</strong>. Recuerde cambiarla en su primer inicio de sesión. Para cambiarla, inicie sesión en la plataforma y vaya a su perfil. Ahí, podrá cambiar su contraseña.
+    </p>
+    <p>
+        Esperemos que tenga una buena experiencia en la plataforma.
+    </p>
 
     <x-mail::footer>
-        <x-mail::signature>
-            Arba
-        </x-mail::signature>
+        Arba
     </x-mail::footer>
 
 </x-mail::message>
