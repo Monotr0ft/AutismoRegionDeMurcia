90c09428f01618d9a3b5b60e436e5f553ad7b435
Monotr0ft <garfremar@gmail.com>
2024-05-02 12:12:22 +0200

    Arreglado, otra vez

--- resources/views/emails/autenticacionarba.blade.php
+++ resources/views/emails/autenticacionarba.blade.php
@@ -4,18 +4,13 @@
         Bienvenido a ARBA
     </x-mail::header>
 
-    <p>
-        Hola {{ $nombre }} {{ $apellidos }}.
-    </p>
-    <p>
-        Se le ha añadido a ARBA como usuario para acceder a la plataforma.
-    </p>
-    <p>
-        Su contraseña es: <strong>{{ $password }}</strong>. Recuerde cambiarla en su primer inicio de sesión. Para cambiarla, inicie sesión en la plataforma y vaya a su perfil. Ahí, podrá cambiar su contraseña.
-    </p>
-    <p>
-        Esperemos que tenga una buena experiencia en la plataforma.
-    </p>
+    Hola {{ $nombre }} {{ $apellidos }}.
+    
+    Se le ha añadido a ARBA como usuario para acceder a la plataforma.
+
+    Su contraseña es: **{{ $password }}**. Recuerde cambiarla en su primer inicio de sesión. Para cambiarla, inicie sesión en la plataforma y vaya a su perfil. Ahí, podrá cambiar su contraseña.
+
+    Esperemos que tenga una buena experiencia en la plataforma.
 
     <x-mail::footer>
         Arba
