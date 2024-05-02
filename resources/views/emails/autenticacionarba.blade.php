90d50d708e60116aed80fae0761df38b5a3bde84
Monotr0ft <garfremar@gmail.com>
2024-05-02 12:19:50 +0200

    Ahora

--- resources/views/emails/autenticacionarba.blade.php
+++ resources/views/emails/autenticacionarba.blade.php
@@ -8,7 +8,7 @@
     
     Se le ha añadido a ARBA como usuario para acceder a la plataforma.
 
-    Su contraseña es: **{{ $password }}**. Recuerde cambiarla en su primer inicio de sesión. Para cambiarla, inicie sesión en la plataforma y vaya a su perfil. Ahí, podrá cambiar su contraseña.
+    Su contraseña es: {{!! $password !!}}. Recuerde cambiarla en su primer inicio de sesión. Para cambiarla, inicie sesión en la plataforma y vaya a su perfil. Ahí, podrá cambiar su contraseña.
 
     Esperemos que tenga una buena experiencia en la plataforma.
 
