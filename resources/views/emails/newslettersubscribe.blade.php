58ac5774ba7a29b1a63bab88712a3f1da6ebc8c9
Monotr0ft <garfremar@gmail.com>
2024-05-23 13:09:20 +0200

    Simplificado

--- resources/views/emails/newslettersubscribe.blade.php
+++ resources/views/emails/newslettersubscribe.blade.php
@@ -3,34 +3,21 @@
 <head>
     <title>Newsletter ARM</title>
 </head>
-<body style="box-sizing: border-box; position: relative; -webkit-text-size-adjust: none; color: #718096; height: 100%; line-height: 1.4; width: 100% !important; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; border-bottom-width: 1px; border-bottom-color: #edf2f7; border-bottom-style: solid; border-top-width: 1px; border-top-color: #edf2f7; border-top-style: solid; margin: 0; padding: 0;" bgcolor="#edf2f7">
-<style>body {
-box-sizing: border-box; position: relative;
-}
-body *:not(html):not(style):not(br):not(tr):not(code) {
-box-sizing: border-box; position: relative;
-}
-body {
--webkit-text-size-adjust: none; background-color: #ffffff; color: #718096; height: 100%; line-height: 1.4; margin: 0; padding: 0; width: 100% !important;
-}
-img {
-max-width: 100%;
-}
-</style>
-    <header style="box-sizing: border-box; position: relative; text-align: center; padding: 25px 0;">
-        <img src="{{ asset('assets/img/Murcia_-_Mapa_municipal.jpg') }}" alt="Logo de ARM" style="box-sizing: border-box; position: relative; max-width: 100%; height: 75px; max-height: 75px; width: 75px;">
+<body style="box-sizing: border-box; position: relative; -webkit-text-size-adjust: none; color: #718096; height: 100%; line-height: 1.4; width: 100% !important; margin: 0; padding: 0; border: 1px solid #edf2f7;">
+    <header style="text-align: center; padding: 25px 0;">
+        <img src="{{ asset('assets/img/Murcia_-_Mapa_municipal.jpg') }}" alt="Logo de ARM" style="max-width: 100%; height: 75px; width: 75px;">
     </header>
 
-    <main style="box-sizing: border-box; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px; background-color: #ffffff; border-radius: 2px; box-shadow: 0 2px 0 rgba(0, 0, 150, 0.025), 2px 4px 0 rgba(0, 0, 150, 0.015); width: 570px; margin: 0 auto; padding: 0; border-color: #e8e5ef; border-width: 1px;">
-        <h1 style="box-sizing: border-box; position: relative; color: #3d4852; font-size: 18px; font-weight: bold; margin-top: 0;" align="left">Bienvenido al boletín de noticias de ARM (Autismo Región de Murcia)</h1>
+    <main style="background-color: #ffffff; border-radius: 2px; box-shadow: 0 2px 0 rgba(0, 0, 150, 0.025), 2px 4px 0 rgba(0, 0, 150, 0.015); width: 570px; margin: 0 auto; border: 1px solid #e8e5ef;">
+        <h1 style="color: #3d4852; font-size: 18px; font-weight: bold; margin-top: 0;" align="left">Bienvenido al boletín de noticias de ARM (Autismo Región de Murcia)</h1>
 
-        <p style="box-sizing: border-box; position: relative; line-height: 1.5em; font-size: 16px; margin-top: 0;" align="left">&iexcl;Gracias por suscribirte a nuestro boletín de noticias! A partir de ahora recibir&aacute;s todas las novedades de nuestra página web.</p>
+        <p style="line-height: 1.5em; font-size: 16px; margin-top: 0;" align="left">&iexcl;Gracias por suscribirte a nuestro boletín de noticias! A partir de ahora recibir&aacute;s todas las novedades de nuestra página web.</p>
 
-        <a href="{{ $url }}" style="box-sizing: border-box; position: relative; color: #fff; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; text-align: center; width: 100%; -webkit-text-size-adjust: none; border-radius: 4px; display: inline-block; overflow: hidden; text-decoration: none; background-color: #2d3748; margin: 30px auto; padding: 0; border-color: #2d3748; border-style: solid; border-width: 8px 18px;">Entra a ver la página web</a>
+        <a href="{{ $url }}" style="color: #fff; text-align: center; width: 100%; border-radius: 4px; display: inline-block; overflow: hidden; text-decoration: none; background-color: #2d3748; margin: 30px auto; padding: 0; border: 8px solid #2d3748;">Entra a ver la página web</a>
     </main>
 
-    <footer style="box-sizing: border-box; position: relative; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px; text-align: center; width: 570px; margin: 0 auto; padding: 0;">
-        Si quieres dejar de recibir correos electrónicos de ARM, puedes hacerlo pulsando <a href="#" style="box-sizing: border-box; position: relative; color: #b0adc5; text-decoration: underline;">aquí</a>.
+    <footer style="text-align: center; width: 570px; margin: 0 auto;">
+        Si quieres dejar de recibir correos electrónicos de ARM, puedes hacerlo pulsando <a href="#" style="color: #b0adc5; text-decoration: underline;">aquí</a>.
     </footer>
 </body>
 </html>
