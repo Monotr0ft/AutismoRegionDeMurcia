29de5d14af96019993349f436cf64e20645df97e
Monotr0ft <garfremar@gmail.com>
2024-10-21 11:11:03 +0200

    Añadido a la imagen del error 404 un hipervínculo al home de la página

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -11,7 +11,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center my-5">
-                <img src="{{ asset('assets/img/Murcia_-_Mapa_municipal.svg') }}" class="img-fluid" width="200" alt="Logo de Autismo Región de Murcia">
+                <a href="{{ route('home') }}"><img src="{{ asset('assets/img/Murcia_-_Mapa_municipal.svg') }}" class="img-fluid" width="200" alt="Logo de Autismo Región de Murcia"></a>
                 <h1>Aquí no hay nada que ver</h1>
             </div>
             <div class="col-12 text-center my-5">
