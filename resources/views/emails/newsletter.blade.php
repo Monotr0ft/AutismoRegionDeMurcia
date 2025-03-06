d5b3543143d466fc4b5bc1b353c6b32460959c3c
Monotr0ft <garfremar@gmail.com>
2025-03-06 13:50:17 +0100

    Voy a quita el app.url

--- resources/views/emails/newsletter.blade.php
+++ resources/views/emails/newsletter.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body style="margin: 0; padding: 0; box-sizing: border-box; -webkit-text-size-adjust: none; color: #718096; line-height: 1.4; width: 100%; height: 100%; border: 1px solid #edf2f7; overflow-x: hidden;">
     <header style="text-align: center; padding-top: 1rem; padding-bottom: 1rem;">
-        <img src="{{ config('app.url') . asset('assets/img/Murcia_-_Mapa_municipal.jpg') }}" alt="Logo de ARM" style="height: 75px; width: 75px; max-width: 100%;">
+        <img src="{{ asset('assets/img/Murcia_-_Mapa_municipal.jpg') }}" alt="Logo de ARM" style="height: 75px; width: 75px; max-width: 100%;">
     </header>
     
     <main style="background: #ffffff; border: 1px solid #e8e5ef; border-radius: 5px; width: 100%; max-width: 570px; margin: 0 auto; padding: 20px;">
