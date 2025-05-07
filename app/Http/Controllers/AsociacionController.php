8e12a059f5c63b7230fb6f8b72dadf2f12cf3c5f
Monotr0ft <garfremar@gmail.com>
2025-05-07 17:51:34 +0200

    Cambiado forma de ordenar

--- app/Http/Controllers/AsociacionController.php
+++ app/Http/Controllers/AsociacionController.php
@@ -16,7 +16,7 @@ class AsociacionController extends Controller
      */
     public function index()
     {
-        $asociaciones = Asociacion::all();
+        $asociaciones = Asociacion::all()->sortBy('nombre');
         return view('autismo.dashboard.paginas.asociaciones.index', ['asociaciones' => $asociaciones]);
     }
 
