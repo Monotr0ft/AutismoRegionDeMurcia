50768f6d47d98e1a7aa2deacf9fb2daf4e9767e3
Monotr0ft <garfremar@gmail.com>
2025-05-23 16:05:36 +0200

    Comprobando si funciona el primer párrafo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Pagina;
+use DOMDocument;
 
 class HomeController extends Controller
 {
@@ -15,8 +16,36 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $parrafo1 = Pagina::where('titulo', 'Home parrafo 1')->first();
-        $parrafo2 = Pagina::where('titulo', 'Home parrafo 2')->first();
-        return view('autismo.paginas.home', ['parrafo1' => $parrafo1, 'parrafo2' => $parrafo2]);
+        $parrafo1 = Pagina::where('titulo', '¿Qué es Autismo Región de Murcia?')->first();
+        $parrafo2 = Pagina::where('titulo', '¿Qué es el Autismo?')->first();
+
+        $primerParrafo = $this->extraerPrimerParrafoConContenido($parrafo1->contenido);
+        $segundoParrafo = $this->extraerPrimerParrafoConContenido($parrafo2->contenido);
+
+        return view('autismo.paginas.home', ['parrafo1' => $primerParrafo, 'parrafo2' => $segundoParrafo]);
+    }
+
+    /**
+     * Extrae el primer párrafo con contenido de un texto HTML.
+     *
+     * @param string $html El contenido HTML del que se extraerá el primer párrafo.
+     * @return string El primer párrafo con contenido.
+     */
+    private function extraerPrimerParrafoConContenido($html)
+    {
+        libxml_use_internal_errors(true); // Ignorar errores de HTML mal formado
+
+        $doc = new DOMDocument();
+        $doc->loadHTML('<?xml encoding="UTF-8">' . $html);
+        $paragraphs = $doc->getElementsByTagName('p');
+
+        foreach ($paragraphs as $paragraph) {
+            $texto = trim($paragraph->textContent);
+            if (!empty($texto) && $texto !== '$nbsp;') {
+                return $doc->saveHTML($paragraph);
+            }
+        }
+
+        return '';
     }
 }
