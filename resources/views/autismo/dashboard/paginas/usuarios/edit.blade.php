eb65ac2b57d0f34b844e1baf9ca2ffa7adf70b9f
Monotr0ft <garfremar@gmail.com>
2025-05-31 18:29:51 +0200

    Añadido el nombre en la parte de editar usuario

--- resources/views/autismo/dashboard/paginas/usuarios/edit.blade.php
+++ resources/views/autismo/dashboard/paginas/usuarios/edit.blade.php
@@ -35,7 +35,7 @@
 
 <div class="row">
     <div class="col-12">
-        <h1 class="text-center">Editar usuario</h1>
+        <h1 class="text-center">Editar usuario {{ $user->name }}</h1>
     </div>
 </div>
 <br>
